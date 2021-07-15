fd5719a202cc0af047e1309e66a0a5f4982c3820
Jake Peters <jpeter61@cord.edu>
2021-07-15 12:57:13 -0500

    hello

--- R Files/FM_Market_Shiny - Jake/server.R
+++ R Files/FM_Market_Shiny - Jake/server.R
@@ -13,52 +13,93 @@ library(tidyverse)
 # Define server logic required to plot house types
 
 shinyServer(function(input, output) {
-  selected <- reactive({
-    input$city
-    input$list_price
-    input$book_section
-    input$sq_ft
-    input$bathrooms
-    input$bedrooms
-    #input$year_built
-    
-    similar_df <- FM_Market_Clean %>%
-      filter(`City` %in% input$city) %>%
-      
-      filter(if(input$list_price[[1]]>100000) `List Price`>=input$list_price[[1]] else TRUE) %>%
-      filter(if(input$list_price[[2]]<1500000) `List Price`<=input$list_price[[2]] else TRUE) %>%
-      
-      filter(`Book Section` %in% input$book_section) %>%
-      
-      filter(if(input$bedrooms[[1]]>0) `Total Bedrooms`>=input$bedrooms[[1]] else TRUE) %>%
-      filter(if(input$bedrooms[[2]]<10) `Total Bedrooms`<=input$bedrooms[[2]] else TRUE) %>%
-      
-      filter(if(input$bathrooms[[1]]>0) `Total Bathrooms`>=input$bathrooms[[1]] else TRUE) %>%
-      filter(if(input$bathrooms[[2]]<10) `Total Bathrooms`<=input$bathrooms[[2]] else TRUE) %>%
-      
-      filter(if(input$sq_ft[[1]]>750) `Total SqFt.`>=input$sq_ft[[1]] else TRUE) %>%
-      filter(if(input$sq_ft[[2]]<5000) `Total SqFt.`<=input$sq_ft[[2]] else TRUE) 
-  })
-
-  output$map <- renderLeaflet({
-    map1 <- leaflet(FM_Market_Clean, options=leafletOptions(preferCanvas=TRUE)) %>% addTiles() %>% fitBounds(~min(`Geo Lon`), ~min(`Geo Lat`), ~max(`Geo Lon`), ~max(`Geo Lat`))
-  })
+  # selected <- reactive({
+  #   input$city
+  #   input$list_price
+  #   input$book_section
+  #   input$sq_ft
+  #   input$bathrooms
+  #   input$bedrooms
+  #   #input$year_built
+  #   
+  #   similar_df <- FM_Market_Clean %>%
+  #     filter(`City` %in% input$city) %>%
+  #     
+  #     filter(if(input$list_price[[1]]>100000) `List Price`>=input$list_price[[1]] else TRUE) %>%
+  #     filter(if(input$list_price[[2]]<1500000) `List Price`<=input$list_price[[2]] else TRUE) %>%
+  #     
+  #     filter(`Book Section` %in% input$book_section) %>%
+  #     
+  #     filter(if(input$bedrooms[[1]]>0) `Total Bedrooms`>=input$bedrooms[[1]] else TRUE) %>%
+  #     filter(if(input$bedrooms[[2]]<10) `Total Bedrooms`<=input$bedrooms[[2]] else TRUE) %>%
+  #     
+  #     filter(if(input$bathrooms[[1]]>0) `Total Bathrooms`>=input$bathrooms[[1]] else TRUE) %>%
+  #     filter(if(input$bathrooms[[2]]<10) `Total Bathrooms`<=input$bathrooms[[2]] else TRUE) %>%
+  #     
+  #     filter(if(input$sq_ft[[1]]>750) `Total SqFt.`>=input$sq_ft[[1]] else TRUE) %>%
+  #     filter(if(input$sq_ft[[2]]<5000) `Total SqFt.`<=input$sq_ft[[2]] else TRUE) 
+  # })
+  # 
+  # output$map <- renderLeaflet({
+  #   map1 <- leaflet(FM_Market_Clean, options=leafletOptions(preferCanvas=TRUE)) %>% addTiles() %>% fitBounds(~min(`Geo Lon`), ~min(`Geo Lat`), ~max(`Geo Lon`), ~max(`Geo Lat`))
+  # })
+  # 
+  # observe({
+  #   leafletProxy("map", data=FM_Market_Clean) %>% clearMarkers() %>% addCircleMarkers(
+  #     lng=selected()$`Geo Lon`, 
+  #     lat=selected()$`Geo Lat`,
+  #     radius=3.5,
+  #     label=lapply(paste0(
+  #       "<img src=",selected()$`Photo URL`,"><br>",
+  #       "List Price: <b>$",comma(selected()$`List Price`, 100),"</b><br>",
+  #       "Our Estimate: <b>$",comma(selected()$`Adjustment Prediction`, 100),"</b><br>",
+  #       selected()$`Book Section`," - ",selected()$`Style`,"<br>",
+  #       selected()$`Total SqFt.`," sq. ft. | ",selected()$`Total Bedrooms`," Bed | ",selected()$`Total Bathrooms`," Bath"),HTML),
+  #     labelOptions=labelOptions(style=list(
+  #       "text-align"="center",
+  #       "font-size"="15px"
+  #     ))
+  #   )
+  # })
+  
 
   observe({
-    leafletProxy("map", data=FM_Market_Clean) %>% clearMarkers() %>% addCircleMarkers(
-      lng=selected()$`Geo Lon`, 
-      lat=selected()$`Geo Lat`,
-      radius=1,
-      label=lapply(paste0(
-        "<img src=",selected()$`Photo URL`,"><br>",
-        "List Price: <b>$",comma(selected()$`List Price`, 100),"</b><br>",
-        "Our Estimate: <b>$",comma(selected()$`Adjustment Prediction`, 100),"</b><br>",
-        selected()$`Book Section`," - ",selected()$`Style`,"<br>",
-        selected()$`Total SqFt.`," sq. ft. / ",selected()$`Total Bedrooms`," Bed / ",selected()$`Total Bathrooms`," Bath"),HTML),
-      labelOptions=labelOptions(style=list(
-        "text-align"="center",
-        "font-size"="15px"
-      ))
-    )
+    mandatoryFilled <-
+      vapply(fieldsMandatory,
+             function(x) {
+               !is.null(input[[x]]) && input[[x]] != ""
+             },
+             logical(1))
+    mandatoryFilled <- all(mandatoryFilled)
+    
+    shinyjs::toggleState(id = "submit", condition = mandatoryFilled)
   })
+  formData <- reactive({
+    input$city1
+    input$sq_ft1
+    input$bathrooms1
+    input$bedrooms1
+
+   # data <- sapply(fieldsAll, function(x) input[[x]])
+    # data <- c(input$city, input$sq_ft)
+    # data
+    data <- c(input$city1, log(as.numeric(input$sqft1)), input$bedrooms1, input$bathrooms1)
+    df <- data.frame(matrix(unlist(data), nrow=1, byrow=TRUE),stringsAsFactors=FALSE)
+    df <- rename(df, "City" = "X1", "Log SqFt" = "X2", "Total Bedrooms" = "X3", "Total Bathrooms" = "X4")
+    df
+    
+  })
+  # output$newdataframe <- renderPrint({
+  #   input$submit
+  #   
+  #   exp(predict(hedonicModel, newdata=df, allow.new.levels=TRUE))
+  # 
+  # })
+ result <- observeEvent(input$submit,{
+    #exp(predict(hedonicModel, newdata=formData(), allow.new.levels=TRUE))
+   View(formData())
+    #formData$`Hedonic Prediction` <- exp(predict(hedonicModel, newdata=formData(), allow.new.levels=TRUE))
+
+  })
+  
 })
--- R Files/FM_Market_Shiny - Jake/ui.R
+++ R Files/FM_Market_Shiny - Jake/ui.R
@@ -17,75 +17,142 @@ shinyUI(
                     inverse = TRUE,
         #inverse = TRUE,
         "Fargo-Moorhead Housing",
+        # tabPanel(
+        #     "House Finder",
+        #     fluidPage(
+        #         sidebarLayout(
+        #             sidebarPanel(
+        #         pickerInput(
+        #             inputId = "city",
+        #             label = "City:",
+        #             choices = unique(na.omit(FM_Market_Clean$`City`)),
+        #             selected = unique(na.omit(FM_Market_Clean$`City`)),
+        #             options = list(`actions-box` = TRUE),
+        #             multiple = TRUE
+        #         ),
+        #         pickerInput(
+        #             inputId = "book_section",
+        #             label = "House Type:", 
+        #             choices = unique(FM_Market_Clean$`Book Section`),
+        #             selected = unique(FM_Market_Clean$`Book Section`),
+        #             options = list(`actions-box` = TRUE), 
+        #             multiple = TRUE
+        #         ),
+        #         sliderInput(
+        #             inputId = "list_price",
+        #             label = "Price Range:",
+        #             min = 100000,
+        #             max = 1500000,
+        #             value = c(100000, 1600000),
+        #             pre="$",
+        #             sep=",",
+        #             step = 5000
+        #         ),
+        #         
+        #         
+        #         sliderInput(
+        #             inputId = "sq_ft",
+        #             label = "Square Footage:",
+        #             min = 750,
+        #             max = 5000,
+        #             value = c(750, 5000),
+        #             sep=",",
+        #             step = 50
+        #         ),
+        #         
+        #         sliderInput(
+        #             inputId = "bedrooms",
+        #             label = "Bedrooms:",
+        #             min = 0,
+        #             max = 10,
+        #             value = c(0,10)
+        #         ),
+        # 
+        #         sliderInput(
+        #             inputId = "bathrooms",
+        #             label = "Bathrooms:",
+        #             min = 0,
+        #             max = 10,
+        #             value = c(0,10),
+        #             step = 0.5,
+        # 
+        #         ),
+        #           width = 3, ),
+        #         
+        #         mainPanel(
+        #         leafletOutput("map"),
+        #         width = 9,
+        #         tags$style(type =  "text/css", "#map {height: calc(100vh - 80px) !important;}")
+        #         )
+        #     )     
+        # )
+        #     ),
+        # tabPanel(
+        #         "House Predictor",
+        #         fluidPage(
+        #                 pickerInput(
+        #                         inputId = "city",
+        #                         label = "Choose Your City:",
+        #                         choices = unique(na.omit(FM_Market_Clean$`City`)),
+        #                         selected = unique(na.omit(FM_Market_Clean$`City`)),
+        #                         options = list(`actions-box` = TRUE),
+        #                         multiple = FALSE
+        #                 ),   
+        #                 textOutput("newdataframe")
+        # )
+        # ),
         tabPanel(
-            "House Finder",
-            fluidPage(
-                sidebarLayout(
-                    sidebarPanel(
-                pickerInput(
-                    inputId = "city",
-                    label = "City:", 
-                    choices = unique(na.omit(FM_Market_Clean$`City`)),
-                    selected = unique(na.omit(FM_Market_Clean$`City`)),
-                    options = list(`actions-box` = TRUE), 
-                    multiple = TRUE
-                ),
-                pickerInput(
-                    inputId = "book_section",
-                    label = "House Type:", 
-                    choices = unique(FM_Market_Clean$`Book Section`),
-                    selected = unique(FM_Market_Clean$`Book Section`),
-                    options = list(`actions-box` = TRUE), 
-                    multiple = TRUE
-                ),
-                sliderInput(
-                    inputId = "list_price",
-                    label = "Price Range:",
-                    min = 100000,
-                    max = 1500000,
-                    value = c(100000, 1600000),
-                    pre="$",
-                    sep=",",
-                    step = 5000
-                ),
-                
-                
-                sliderInput(
-                    inputId = "sq_ft",
-                    label = "Square Footage:",
-                    min = 750,
-                    max = 5000,
-                    value = c(750, 5000),
-                    sep=",",
-                    step = 50
-                ),
-                
-                sliderInput(
-                    inputId = "bedrooms",
-                    label = "Bedrooms:", 
-                    min = 0,
-                    max = 10,
-                    value = c(0,10)
-                ),
-                
-                sliderInput(
-                    inputId = "bathrooms",
-                    label = "Bathrooms:",
-                    min = 0,
-                    max = 10,
-                    value = c(0,10),
-                    step = 0.5
+                "Form",
+                fluidPage(
+                        shinyjs::useShinyjs(),
+                        shinyjs::inlineCSS(appCSS),
+                        titlePanel("Mimicking a Google Form with a Shiny app"),
+                        
+                        div(
+                                id = "form",
+                                
+                               # textInput("name", labelMandatory("Name"), ""),
+                                pickerInput(
+                                        inputId = "city1",
+                                        label =("Choose Your City:"),
+                                        choices = unique(na.omit(FM_Market_Clean$`City`)),
+                                        selected = unique(na.omit(FM_Market_Clean$`City`)),
+                                        options = list(`actions-box` = TRUE),
+                                        multiple = FALSE
+                                ), 
+                                sliderInput(
+                                        inputId = "sq_ft1",
+                                        label = "Square Footage:",
+                                        min = 200,
+                                        max = 10000,
+                                        value = c(3000),
+                                        sep=",",
+                                        step = 50
+                                ),
+                                sliderInput(
+                                        inputId = "bedrooms1",
+                                        label = "Bedrooms:",
+                                        min = 0,
+                                        max = 10,
+                                        value = 5
+                                ),
+                                
+                                sliderInput(
+                                        inputId = "bathrooms1",
+                                        label = "Bathrooms:",
+                                        min = 0,
+                                        max = 10,
+                                        value = 5,
+                                        step = 0.5,
+                                        
+                                ),
+                                
+                                actionButton("submit", "Submit", class = "btn-primary"),
+                                renderPrint("newdataframe")
+                        ),
                 )
-                    ),
-                
-                mainPanel(
-                leafletOutput("map")
-                )
-            )     
         )
-            )
-            )
+        )
 )
-
 
 
