2b0ac58e31955cc038f47d7ce8b9490dd06e4b3c
alexmivoigt <46804890+alexmivoigt@users.noreply.github.com>
2021-07-15 12:58:32 -0500

    Archived old files, added map controls/filter reset

--- R Files/FM_Market_Shiny/global.R
+++ R Files/FM_Market_Shiny/global.R
@@ -4,13 +4,25 @@ FM_Market_Clean <- read_csv("FM_Market_Clean.csv")
 
 FM_Market_Clean[is.na(FM_Market_Clean$`Adjustment Prediction`),]$`Adjustment Prediction` <- FM_Market_Clean[is.na(FM_Market_Clean$`Adjustment Prediction`),]$`List Price`
 
-minlon <- min(FM_Market_Clean$`Geo Lon`)
-minlat <- min(FM_Market_Clean$`Geo Lat`)
-maxlon <- max(FM_Market_Clean$`Geo Lon`)
-maxlat <- max(FM_Market_Clean$`Geo Lat`)
+min_lon <- min(FM_Market_Clean$`Geo Lon`)
+min_lat <- min(FM_Market_Clean$`Geo Lat`)
+max_lon <- max(FM_Market_Clean$`Geo Lon`)
+max_lat <- max(FM_Market_Clean$`Geo Lat`)
+
+min_listprice <- 100000
+max_listprice <- 1100000
+
+min_sqft <- 1000
+max_sqft <- 6000
+
+min_bedrooms <- 1
+max_bedrooms <- 8
+
+min_bathrooms <- 1
+max_bathrooms <- 8
+
+min_yearbuilt <- 1900
+max_yearbuilt <- 2021
 
 FM_Market_Clean[FM_Market_Clean$`Book Section`=="Twinhomes",]$`Book Section`<-"Twinhome"
-FM_Market_Clean[is.na(FM_Market_Clean$`Book Section`),]$`Book Section` <- "Other"
-
 FM_Market_Clean[FM_Market_Clean$`Style`=="1 1/2 Stor",]$`Style` <- "1&frac12 Story"
-FM_Market_Clean[is.na(FM_Market_Clean$`Style`),]$`Book Section` <- "Other"
--- R Files/FM_Market_Shiny/server.R
+++ R Files/FM_Market_Shiny/server.R
@@ -4,7 +4,8 @@ library(scales)
 library(tidyverse)
 library(raster)
 
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
+  
   selected <- reactive({
     input$city
     input$book_section
@@ -13,33 +14,39 @@ shinyServer(function(input, output) {
     input$sq_ft
     input$bathrooms
     input$bedrooms
+    input$year_built
     
     similar_df <- FM_Market_Clean %>%
       filter(`City` %in% input$city) %>%
       filter(`Book Section` %in% input$book_section) %>%
       filter(`Style` %in% input$style) %>%
       
-      filter(if(input$list_price[[1]]>100000) `List Price`>=input$list_price[[1]] else TRUE) %>%
-      filter(if(input$list_price[[2]]<1600000) `List Price`<=input$list_price[[2]] else TRUE) %>%
+      filter(if(input$list_price[[1]]>min_listprice) `List Price`>=input$list_price[[1]] else TRUE) %>%
+      filter(if(input$list_price[[2]]<max_listprice) `List Price`<=input$list_price[[2]] else TRUE) %>%
       
-      filter(if(input$bedrooms[[1]]>1) `Total Bedrooms`>=input$bedrooms[[1]] else TRUE) %>%
-      filter(if(input$bedrooms[[2]]<8) `Total Bedrooms`<=input$bedrooms[[2]] else TRUE) %>%
+      filter(if(input$bedrooms[[1]]>min_bedrooms) `Total Bedrooms`>=input$bedrooms[[1]] else TRUE) %>%
+      filter(if(input$bedrooms[[2]]<max_bedrooms) `Total Bedrooms`<=input$bedrooms[[2]] else TRUE) %>%
       
-      filter(if(input$bathrooms[[1]]>1) `Total Bathrooms`>=input$bathrooms[[1]] else TRUE) %>%
-      filter(if(input$bathrooms[[2]]<8) `Total Bathrooms`<=input$bathrooms[[2]] else TRUE) %>%
+      filter(if(input$bathrooms[[1]]>min_bathrooms) `Total Bathrooms`>=input$bathrooms[[1]] else TRUE) %>%
+      filter(if(input$bathrooms[[2]]<max_bathrooms) `Total Bathrooms`<=input$bathrooms[[2]] else TRUE) %>%
       
-      filter(if(input$sq_ft[[1]]>1000) `Total SqFt.`>=input$sq_ft[[1]] else TRUE) %>%
-      filter(if(input$sq_ft[[2]]<6000) `Total SqFt.`<=input$sq_ft[[2]] else TRUE) 
+      filter(if(input$sq_ft[[1]]>min_sqft) `Total SqFt.`>=input$sq_ft[[1]] else TRUE) %>%
+      filter(if(input$sq_ft[[2]]<max_sqft) `Total SqFt.`<=input$sq_ft[[2]] else TRUE) %>%
+      
+      filter(if(input$year_built[[1]]>min_yearbuilt) `Year Built`>=input$year_built[[1]] else TRUE) %>%
+      filter(if(input$year_built[[2]]<max_yearbuilt) `Year Built`<=input$year_built[[2]] else TRUE)
   })
 
   output$map <- renderLeaflet({
     map1 <- leaflet(data=FM_Market_Clean, options=leafletOptions(preferCanvas=TRUE)) %>%
       addTiles() %>%
-      fitBounds(minlon, minlat, maxlon, maxlat) %>%
+      fitBounds(min_lon, min_lat, max_lon, max_lat) %>%
       addEasyButton(easyButton(
         icon = "fa-crosshairs",
-        title="Reset zoom",
-        onClick = JS(paste0("function(btn, map){ map.fitBounds([[",minlat,",",minlon,"],[",maxlat,",",maxlon,"]]);}"))
+        title="Recenter map",
+        onClick = JS(
+          "function(btn, map) {map.fitBounds(map.layerManager.getLayerGroup('selected').getBounds(), {padding: [1,1]});}"
+        ) 
       ))
   })
 
@@ -47,7 +54,8 @@ shinyServer(function(input, output) {
     leafletProxy("map", data=FM_Market_Clean) %>% clearMarkers() %>% addCircleMarkers(
       lng=selected()$`Geo Lon`, 
       lat=selected()$`Geo Lat`,
-      radius=1,
+      radius=2,
+      group="selected",
       label=lapply(paste0(
         "<img src=",selected()$`Photo URL`,"><br>",
         "List Price: <b>$",comma(selected()$`List Price`, 100),"</b><br>",
@@ -59,5 +67,84 @@ shinyServer(function(input, output) {
         "font-size"="15px"
       ))
     )
+  })
+  
+  observeEvent(input$reset,{
+    updatePickerInput(
+      session = session,
+      inputId = "city",
+      label = "City:", 
+      choices = unique(na.omit(FM_Market_Clean$`City`)),
+      selected = unique(na.omit(FM_Market_Clean$`City`)),
+      options = list(`actions-box` = TRUE)
+    )
+      
+    updatePickerInput(
+      session = session,
+      inputId = "book_section",
+      label = "House Type:", 
+      choices = unique(FM_Market_Clean$`Book Section`),
+      selected = unique(FM_Market_Clean$`Book Section`),
+      options = list(`actions-box` = TRUE)
+    )
+      
+    updatePickerInput(
+      session = session,
+      inputId = "style",
+      label = "House style:",
+      choices = c("1 Story", "1 1/2 Story"="1&frac12 Story", "2 Story", "3 Story", "Bi Level", "3 Level", "4 Level"),
+      selected = unique(FM_Market_Clean$`Style`),
+      options = list(`actions-box` = TRUE)
+    )
+      
+    updateSliderInput(
+      session = session,
+      inputId = "list_price",
+      label = "Price Range:",
+      min = min_listprice,
+      max = max_listprice,
+      value = c(min_listprice, max_listprice),
+      step=5000
+    )
+      
+    updateSliderInput(
+      session = session,
+      inputId = "sq_ft",
+      label = "Square Footage:",
+      min = min_sqft,
+      max = max_sqft,
+      value = c(min_sqft, max_sqft),
+      step=50
+    )
+      
+    updateSliderInput(
+      session = session,
+      inputId = "bedrooms",
+      label = "Bedrooms:", 
+      min = min_bedrooms,
+      max = max_bedrooms,
+      value = c(min_bedrooms, max_bedrooms)
+    )
+      
+    updateSliderInput(
+      session = session,
+      inputId = "bathrooms",
+      label = "Bathrooms:",
+      min = min_bathrooms,
+      max = max_bathrooms,
+      value = c(min_bathrooms, max_bathrooms),
+      step = 0.5
+    )
+      
+    updateSliderInput(
+      session = session,
+      inputId = "year_built",
+      label = "Year Built:",
+      min = min_yearbuilt,
+      max = max_yearbuilt,
+      value = c(min_yearbuilt, max_yearbuilt)
+    )
+      
+    leafletProxy("map", data=FM_Market_Clean) %>% fitBounds(min_lon, min_lat, max_lon, max_lat)
   })
 })
